108bb024f16c50b2fa99b8bb96fff821347592d7
kikk01 <hugobar@live.fr>
2022-01-02 19:40:24 +0100

    fix cost by supports constant

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
         1 => ['amount' => 500, 'costType' => 1, 'costAmount' => 1.99],
         2 => ['amount' => 3330, 'costType' => 0, 'costAmount' => 0.5],
         3 => ['amount' => 7750, 'costType' => 1, 'costAmount' => 16.65],
-        4 => ['amount' => 1000000000, 'costType' => 0, 'costAmount' => 0.22]
+        4 => ['amount' => 0, 'costType' => 0, 'costAmount' => 0.22]
     ];
 
     $rate = (int) $_GET['rate'];
-    $placement = (int) $_GET['placement'];
+    $placement = (float) $_GET['placement'];
     $numberYear = (int) isset($_GET['year']) ? $_GET['year'] : 1;
     $numberDay = 254*$numberYear;
     $i = 0;
@@ -25,7 +25,7 @@
                 ////////  Happy new ' . $i/254 . "years //////////
             </p>" : 
             '';
-            
+
         echo  nl2br("jour " . $i . ": " . $placement  .  "\n");
         $i++;
     }
@@ -41,18 +41,20 @@
     function getCostSupport(): array
     {
         global $placement;
-        return max(array_filter(
+        $supportsOverPlacementArray = array_filter(
             COST_BY_SUPPORTS, function($supports) use($placement) {
                 return $supports['amount'] > $placement;
             }
-        ));
+        );
+
+        return sizeof($supportsOverPlacementArray ) === 0 ? COST_BY_SUPPORTS[4] : min($supportsOverPlacementArray);
     }
 
     function calculPlacementAfterOrderCost(): void
     {
         global $placement, $costSupports;
         $placement = $costSupports['costType'] === 0 ? 
-            $placement - ($placement*$costSupports['costAmount']/100) :
+            $placement - round($placement*$costSupports['costAmount']/100, 2, PHP_ROUND_HALF_DOWN) :
             $placement - $costSupports['costAmount']
         ;
     }
@@ -60,7 +62,7 @@
     function calculGain(): void
     {
         global $placement, $rate;
-        $placement = $placement + ($placement*$rate/100);
+        $placement = round($placement + ($placement*$rate/100), 2, PHP_ROUND_HALF_DOWN);
     }
 
 ?>
